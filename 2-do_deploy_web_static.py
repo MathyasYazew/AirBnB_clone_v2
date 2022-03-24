7de1e9c74a56cd5cd6d8b3141b14b6da9414aae8
MathyasYazew <mathyasyazew@gmail.com>
2022-03-24 11:15:09 -0700

    Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
 """
 
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+from fabric.api import *
+from datetime import datetime
+import os
+
+filename = "web_static_" + datetime.now().replace(microsecond=0)\
+            .strftime("%Y%m%d%H%M%S") + ".tgz"
+
+env.hosts = ["ubuntu@34.138.32.10", "ubuntu@34.237.53.89"]
+
+
+def do_pack():
+    """Fabric function"""
+    print("Packing web_static to versions/{}".format(filename))
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    local("tar -cvzf versions/{} web_static".format(filename))
+    print("web_static packed: versions/{} -> {}Bytes".format(
+        filename, os.path.getsize("versions/{}".format(filename))))
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Fabric deploy function, takes a file path as argument"""
+    if not os.path.isfile(archive_path):
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+    # remove .tgz extension
+    filename = archive_path.replace("versions/", "")
+    # remove versions folder path
+    folder_name = filename.replace(".tgz", "")
+    put(archive_path, "/tmp", use_sudo=True)
+    run("mkdir -p /data/web_static/releases/{}".format(folder_name))
+    run("tar -xzf /tmp/{} --strip-components 1 -C \
+        /data/web_static/releases/{}".format(filename, folder_name))
+    run("rm /tmp/{}".format(filename))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{} /data/web_static/current".
+        format(folder_name))
